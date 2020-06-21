88f6c745d23217ce22e1e8224c67462f047a29f0
Johannes <johannes.bostelmann@tuhh.de>
2020-06-21 11:57:39 +0200

    changed featurization

--- src/featurization.py
+++ src/featurization.py
@@ -7,14 +7,23 @@ import numpy as np
 import pandas as pd
 # BeautifulSoup is used to remove html tags from the text
 from bs4 import BeautifulSoup
-import re # For regular expressions
-
+import re  # For regular expressions
+from gensim.models import word2vec
+import nltk
+import logging
+import pickle
 # Stopwords can be useful to undersand the semantics of the sentence.
 # Therefore stopwords are not removed while creating the word2vec model.
 # But they will be removed  while averaging feature vectors.
 from nltk.corpus import stopwords
 import os
 import sys
+import errno
+# word2vec expects a list of lists.
+# Using punkt tokenizer for better splitting of a paragraph into sentences.
+nltk.download('punkt')
+nltk.download('stopwords')
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
 
 def mkdir_p(path):
@@ -25,22 +34,12 @@ def mkdir_p(path):
             pass
         else:
             raise
-            
-# Read data from files
-
-input = sys.argv[1]
-output = sys.argv[2]
-mkdir_p(sys.argv[2])
-path_train = os.path.join(input, 'Train.csv')
-path_test = os.path.join(input, 'Test.csv')
-train = pd.read_csv('data/prepared/Train.csv')
-test = pd.read_csv('data/prepared/Test.csv')
 
 
 # This function converts a text to a sequence of words.
 def review_wordlist(review, remove_stopwords=False):
     # 1. Removing html tags
-    review_text = BeautifulSoup(review,"html.parser").get_text()
+    review_text = BeautifulSoup(review, "html.parser").get_text()
     # 2. Removing non-letter.
     review_text = re.sub("[^a-zA-Z]", " ", review_text)
     # 3. Converting to lower case and splitting
@@ -53,15 +52,6 @@ def review_wordlist(review, remove_stopwords=False):
     return (words)
 
 
-
-# word2vec expects a list of lists.
-# Using punkt tokenizer for better splitting of a paragraph into sentences.
-
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-
 # This function splits a review into sentences
 def review_sentences(review, tokenizer, remove_stopwords=False):
     # 1. Using nltk tokenizer
@@ -69,103 +59,115 @@ def review_sentences(review, tokenizer, remove_stopwords=False):
     sentences = []
     # 2. Loop for each sentence
     for raw_sentence in raw_sentences:
-        if len(raw_sentence)>0:
-            sentences.append(review_wordlist(raw_sentence,\
-                                            remove_stopwords))
+        if len(raw_sentence) > 0:
+            sentences.append(review_wordlist(raw_sentence, \
+                                             remove_stopwords))
 
     # This returns the list of lists
     return sentences
 
-sentences = []
-print("Parsing sentences from training set")
-for review in train["text"]:
-    sentences += review_sentences(review, tokenizer)
 
-# Importing the built-in logging module
-import logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-# Creating the model and setting values for the various parameters, To do: finetuning 
-num_features = 300  # Word vector dimensionality
-min_word_count = 40 # Minimum word count
-num_workers = 4     # Number of parallel threads
-context = 10        # Context window size
-downsampling = 1e-3 # (0.001) Downsample setting for frequent words
-
-# Initializing the train model
-from gensim.models import word2vec
-print("Training model....")
-model = word2vec.Word2Vec(sentences,\
-                          workers=num_workers,\
-                          size=num_features,\
-                          min_count=min_word_count,\
-                          window=context,
-                          sample=downsampling)
-
-# To make the model memory efficient
-model.init_sims(replace=True)
-
-# Saving the model for later use. Can be loaded using Word2Vec.load()
-model_name = "300features_40minwords_10context"
-model_path = os.path.join(output, model_name)
-model.save(model_path)
-print(model.wv.most_similar("crime"))
-print(model.wv.most_similar("beer"))
-print(model.wv.most_similar("math"))
-# This will give the total number of words in the vocabolary created from this dataset
-model.wv.syn0.shape
 # Function to average all word vectors in a paragraph
 def featureVecMethod(words, model, num_features):
     # Pre-initialising empty numpy array for speed
-    featureVec = np.zeros(num_features,dtype="float32")
+    featureVec = np.zeros(num_features, dtype="float32")
     nwords = 0
-    
-    #Converting Index2Word which is a list to a set for better speed in the execution.
+    list = []
+    # Converting Index2Word which is a list to a set for better speed in the execution.
     index2word_set = set(model.wv.index2word)
-    
-    for word in  words:
+
+    for word in words:
         if word in index2word_set:
             nwords = nwords + 1
-            featureVec = np.add(featureVec,model[word])
-    
-    # Dividing the result by number of words to get average
-    featureVec = np.divide(featureVec, nwords)
-    return featureVec
-    # Function for calculating the average feature vector
+            list.append(model[word])
+
+    return list
+
+
 def getAvgFeatureVecs(reviews, model, num_features):
     counter = 0
-    reviewFeatureVecs = np.zeros((len(reviews),num_features),dtype="float32")
+    listoflist = [] # Maybe as np array np.zeros((len(reviews), num_features), dtype="float32")
     for review in reviews:
         # Printing a status message every 1000th review
-        if counter%1000 == 0:
-            print("Review %d of %d"%(counter,len(reviews)))
-            
-        reviewFeatureVecs[counter] = featureVecMethod(review, model, num_features)
-        counter = counter+1
+        if counter % 1000 == 0:
+            print("Review %d of %d" % (counter, len(reviews)))
+
+        listoflist.append(featureVecMethod(review, model, num_features))
+        counter = counter + 1
+
+    return listoflist
+
+
+if __name__ == '__main__':
+    # Read data from files
+
+    input = sys.argv[1]
+    output = sys.argv[2]
+    mkdir_p(sys.argv[2])
+    path_train = os.path.join(input, 'Train.csv')
+    path_test = os.path.join(input, 'Test.csv')
+    train = pd.read_csv('data/prepared/Train.csv')
+    test = pd.read_csv('data/prepared/Test.csv')
+
+    sentences = []
+    print("Parsing sentences from training set")
+    for review in train["text"]:
+        sentences += review_sentences(review, tokenizer)
+
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    # Creating the model and setting values for the various parameters, To do: finetuning
+    num_features = 300  # Word vector dimensionality
+    min_word_count = 40  # Minimum word count
+    num_workers = 4  # Number of parallel threads
+    context = 10  # Context window size
+    downsampling = 1e-3  # (0.001) Downsample setting for frequent words
+
+    # Initializing the train model
+
+    print("Training model....")
+    model = word2vec.Word2Vec(sentences, \
+                              workers=num_workers, \
+                              size=num_features, \
+                              min_count=min_word_count, \
+                              window=context,
+                              sample=downsampling)
+
+    # To make the model memory efficient
+    model.init_sims(replace=True)
+
+    # Saving the model for later use. Can be loaded using Word2Vec.load()
+    model_name = "300features_40minwords_10context"
+    model_path = os.path.join(output, model_name)
+    model.save(model_path)
+    print(model.wv.most_similar("crime"))
+    print(model.wv.most_similar("beer"))
+    print(model.wv.most_similar("math"))
+    # This will give the total number of words in the vocabolary created from this dataset
+    model.wv.syn0.shape
+
+    # Calculating average feature vector for training set
+    clean_train_reviews = []
+    for review in train['text']:
+        clean_train_reviews.append(review_wordlist(review, remove_stopwords=True))
+
+    trainDataVecs=getAvgFeatureVecs(clean_train_reviews, model, num_features)
+    train_path = os.path.join(output, 'trainDataVec.pickle')
+    print(type(trainDataVecs))
+    with open(train_path, 'wb') as f:
+     pickle.dump(trainDataVecs, f)
         
-    return reviewFeatureVecs
+
+    # Calculating average feature vactors for test set
+    clean_test_reviews = []
+    for review in test["text"]:
+        clean_test_reviews.append(review_wordlist(review, remove_stopwords=True))
+
+    testDataVecs=getAvgFeatureVecs(clean_test_reviews, model, num_features)
+    test_path = os.path.join(output, 'testDataVec')
+    with open(test_path, 'wb') as f:
+        pickle.dump(testDataVecs, f)
     
-
-# Calculating average feature vector for training set
-clean_train_reviews = []
-for review in train['text']:
-    clean_train_reviews.append(review_wordlist(review, remove_stopwords=True))
-
-trainDataVecs = getAvgFeatureVecs(clean_train_reviews, model, num_features)
-train_path = os.path.join(output, 'trainDataVec')
-np.savetxt(train_path, trainDataVecs, delimiter=",")
-
-# Calculating average feature vactors for test set
-clean_test_reviews = []
-for review in test["text"]:
-    clean_test_reviews.append(review_wordlist(review, remove_stopwords=True))
-
-testDataVecs = getAvgFeatureVecs(clean_test_reviews, model, num_features)
-test_path = os.path.join(output, 'testDataVec')
-np.savetxt(test_path, testDataVecs, delimiter=",")
-    
-
-
 
 
 
